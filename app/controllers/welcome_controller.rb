8764f7d32037df835d1a4a34484a14c8e2e5c053
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-06 15:06:46 +0900

    indexをpostクラスにした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-
+  	  render :text => params
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  post 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
