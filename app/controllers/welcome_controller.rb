55dccec11ae9cbf2cfb7097e4ae642d781e19caf
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-07 15:43:52 +0900

    コントローラ変更

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,12 +9,12 @@ class WelcomeController < ApplicationController
 	  json_request = JSON.parse(request.body.read)
 
 	  # パース後のデータを表示
-	  p "json_request => #{json_request}"
-	  p "#{json_request.to_hash}"
+	  #p "json_request => #{json_request}"
+	  #p "#{json_request.to_hash}"
 
 	  # 各要素へのアクセス方法
-	  p "data => #{json_request["data"]}"
-	  p "data.type => #{json_request["data"]["type"]}"
+	  #p "data => #{json_request["data"]}"
+	  #p "data.type => #{json_request["data"]["type"]}"
   end
 
 end
