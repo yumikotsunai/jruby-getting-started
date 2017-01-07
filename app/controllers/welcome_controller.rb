868dcf33d8c778098f6731b765af26a67a2faecf
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-07 17:52:12 +0900

    aa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,20 @@
 require 'json'
+require 'logger'
+
 class WelcomeController < ApplicationController
 	
   # この↓一文がないとCSRFチェックでこけるので、APIをやりとりしているControllerには必要
   skip_before_filter :verify_authenticity_token
+  
+  #logインスタンス生成
+  log = Logger.new('C:\Users\tsunai\Documents\WebHook\jruby-getting-started\log\debug.log')
 
   # GET /welcome
   def index
   	  # 読み込み時に一度パースが必要
 	  json_request = JSON.parse(request.body.read)
+	  log.debug('test')
+	  log.debug(json_request)
 	  
       if !json_request.blank?
       	personal = json_request
