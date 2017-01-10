85ed0f901807da06a80aa0264d42bdab625f06eb
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-10 17:53:15 +0900

    aa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,17 +12,18 @@ class WelcomeController < ApplicationController
   	  # 読み込み時に一度パースが必要
 	  json_request = JSON.parse(request.body.read)
 	  
-	  #logインスタンス生成
-  	  log = Logger.new('C:\Users\tsunai\Documents\WebHook\jruby-getting-started\log\debug.log')
-	  log.debug('test')
-	  log.debug(json_request)
+	  #logインスタンス生成 ⇒これは効いていないよう
+  	  #log = Logger.new('C:\Users\tsunai\Documents\WebHook\jruby-getting-started\log\debug.log')
+	  #log.debug('test')
+	  #log.debug(json_request)
 	  
-      if !json_request.blank?
-      	personal = json_request
-      else
-      	personal = {'status' => 500}
-      end
-
+      #if !json_request.blank?
+      #	personal = json_request
+      #else
+      #	personal = {'status' => 500}
+      #end
+      
+      personal = {'status' => 500}
       render :json => personal
       
       
