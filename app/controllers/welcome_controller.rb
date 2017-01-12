4d0bc25ea970e4f6f2deb3a8f90ef120e5ec4722
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-12 16:18:03 +0900

    aa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,48 +11,50 @@ class WelcomeController < ApplicationController
   	  # 読み込み時に一度パースが必要
 	  #json_request = JSON.parse(request.body.read)
 	  
-	  #print(1);
-	  #print(request["data"]);
-	  puts(2);
-	  data = request["data"];
+	  if !request.body.read.blank?
 	  
-	  type = data["type"];
-	  
-	  attributes = data["attributes"];
-	  source = attributes["source"];
-	  status = attributes["status"];
-	  time_zone = attributes["time_zone"];
-	  occurred_at = attributes["occurred_at"];
-	  created_at = attributes["created_at"];
-	  updated_at = attributes["updated_at"];
-	  nmethod = attributes["method"];
-	  publisher_id = attributes["publisher_id"];
-	  publisher_type = attributes["publisher_type"];
-	  source = attributes["source"];
-	  
-	  nid = data["id"];
-	  
-	  links = data["links"];
-	  nself = links["self"];
-	  publisher = links["publisher"];
-	  
-	  puts(data);
-	  puts(type);
-	  puts(attributes);
-	  puts(source);
-	  puts(status);
-	  puts(time_zone);
-	  puts(occurred_at);
-	  puts(created_at);
-	  puts(updated_at);
-	  puts(nmethod);
-	  puts(publisher_id);
-	  puts(publisher_type);
-	  puts(source);
-	  puts(nid);
-	  puts(links);
-	  puts(nself);
-	  puts(publisher);
+		  data = request["data"];
+		  
+		  type = data["type"];
+		  
+		  attributes = data["attributes"];
+		  source = attributes["source"];
+		  status = attributes["status"];
+		  time_zone = attributes["time_zone"];
+		  occurred_at = attributes["occurred_at"];
+		  created_at = attributes["created_at"];
+		  updated_at = attributes["updated_at"];
+		  nmethod = attributes["method"];
+		  publisher_id = attributes["publisher_id"];
+		  publisher_type = attributes["publisher_type"];
+		  source = attributes["source"];
+		  
+		  nid = data["id"];
+		  
+		  links = data["links"];
+		  nself = links["self"];
+		  publisher = links["publisher"];
+		  
+		  puts(data);
+		  puts(type);
+		  puts(attributes);
+		  puts(source);
+		  puts(status);
+		  puts(time_zone);
+		  puts(occurred_at);
+		  puts(created_at);
+		  puts(updated_at);
+		  puts(nmethod);
+		  puts(publisher_id);
+		  puts(publisher_type);
+		  puts(source);
+		  puts(nid);
+		  puts(links);
+		  puts(nself);
+		  puts(publisher);
+		  
+	  else
+	  end
 	  
 	  render :text => 'イベント'
 	  
