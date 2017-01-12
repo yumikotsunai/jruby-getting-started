5f1d43651f586dadae687371df02e3cca97d852a
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-12 17:42:11 +0900

    aa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,45 +13,48 @@ class WelcomeController < ApplicationController
 	  
 	  if !request.body.read.blank?
 	  
-		  data = request["data"];
+		  #data = request["data"];
+		  data = request[:data];
+		  #type = data["type"];
+		  type = data[:type];
 		  
-		  type = data["type"];
+		  #attributes = data["attributes"];
+		  #source = attributes["source"];
+		  #status = attributes["status"];
+		  #time_zone = attributes["time_zone"];
+		  #occurred_at = attributes["occurred_at"];
+		  #created_at = attributes["created_at"];
+		  #updated_at = attributes["updated_at"];
+		  #nmethod = attributes["method"];
+		  #publisher_id = attributes["publisher_id"];
+		  #publisher_type = attributes["publisher_type"];
+		  #source = attributes["source"];
 		  
-		  attributes = data["attributes"];
-		  source = attributes["source"];
-		  status = attributes["status"];
-		  time_zone = attributes["time_zone"];
-		  occurred_at = attributes["occurred_at"];
-		  created_at = attributes["created_at"];
-		  updated_at = attributes["updated_at"];
-		  nmethod = attributes["method"];
-		  publisher_id = attributes["publisher_id"];
-		  publisher_type = attributes["publisher_type"];
-		  source = attributes["source"];
+		  #nid = data["id"];
 		  
-		  nid = data["id"];
+		  #links = data["links"];
+		  #nself = links["self"];
+		  #publisher = links["publisher"];
 		  
-		  links = data["links"];
-		  nself = links["self"];
-		  publisher = links["publisher"];
 		  
 		  puts(data);
-		  puts(type);
-		  puts(attributes);
-		  puts(source);
-		  puts(status);
-		  puts(time_zone);
-		  puts(occurred_at);
-		  puts(created_at);
-		  puts(updated_at);
-		  puts(nmethod);
-		  puts(publisher_id);
-		  puts(publisher_type);
-		  puts(source);
-		  puts(nid);
-		  puts(links);
-		  puts(nself);
-		  puts(publisher);
+		  #puts(type);
+		  #puts(attributes);
+		  #puts(source);
+		  #puts(status);
+		  #puts(time_zone);
+		  #puts(occurred_at);
+		  #puts(created_at);
+		  #puts(updated_at);
+		  #puts(nmethod);
+		  #puts(publisher_id);
+		  #puts(publisher_type);
+		  #puts(source);
+		  #puts(nid);
+		  #puts(links);
+		  #puts(nself);
+		  #puts(publisher);
+		  
 		  
 		  #render :text => 'イベント : ' + data
 		  #render :text => 'ソース : ' + source
