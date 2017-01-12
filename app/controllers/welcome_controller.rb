bed76dcd07f7af155e97f20834e0899e76995d5c
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-12 15:39:51 +0900

    パラメータを追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,12 +11,32 @@ class WelcomeController < ApplicationController
   	  # 読み込み時に一度パースが必要
 	  #json_request = JSON.parse(request.body.read)
 	  
-	  
 	  #print(1);
 	  #print(request["data"]);
-	  print(2);
+	  puts(2);
 	  data = request["data"];
-	  print(data["type"]);
+	  
+	  type = data["type"];
+	  
+	  attributes = data["attributes"];
+	  source = attributes["source"];
+	  status = attributes["status"];
+	  time_zone = attributes["time_zone"];
+	  occurred_at = attributes["occurred_at"];
+	  created_at = attributes["created_at"];
+	  updated_at = attributes["updated_at"];
+	  nmethod = attributes["method"];
+	  publisher_id = attributes["publisher_id"];
+	  publisher_type = attributes["publisher_type"];
+	  source = attributes["source"];
+	  
+	  nid = data["id"];
+	  
+	  links = data["links"];
+	  nself = links["self"];
+	  publisher = links["publisher"];
+	  
+	  puts(data);
 	  
 	  #render :text => "#{data["type"]}"
 	  
