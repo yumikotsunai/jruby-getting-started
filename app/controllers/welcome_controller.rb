7fdd0e29621a81d54f57738f73c56f22550ff8b7
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-19 17:43:33 +0900

    aa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,14 +7,13 @@ class WelcomeController < ApplicationController
   # この↓一文がないとCSRFチェックでこけるので、APIをやりとりしているControllerには必要
   skip_before_filter :verify_authenticity_token
   
+  apikey = 'AIzaSyANPVaEQ0I4erpjEzzcJFMHG8-EcABMoao'
+  calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
+  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
   
   # GET /welcome
   def index
   	  
-  	  apikey = 'AIzaSyANPVaEQ0I4erpjEzzcJFMHG8-EcABMoao'
-  	  calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
-  	  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
-	  
 	  if !request.body.read.blank?
 	      
 	      puts(googleテスト2)
