ca9dd9db0688842d6c5020511cc968906425ec68
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-23 15:37:02 +0900

    gテスト

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,29 @@
 require 'open-uri'
 require 'cgi'
 require 'json'
+require 'google/api_client'
 
 class WelcomeController < ApplicationController
   
   # この↓一文がないとCSRFチェックでこけるので、APIをやりとりしているControllerには必要
   skip_before_filter :verify_authenticity_token
   
-  apikey = 'AIzaSyANPVaEQ0I4erpjEzzcJFMHG8-EcABMoao'
-  calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
+  #apikey = 'AIzaSyANPVaEQ0I4erpjEzzcJFMHG8-EcABMoao'
+  apikey = 'AIzaSyAC-jdWZDppBm8_cP1LRA79QeChfgPr9Qs'
+  #calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
+  calendarId = 'i8a77r26f9pu967g3pqpubv0ng@group.calendar.google.com'
   
   # GET /welcome
   def index
   	  
 	  if !request.body.read.blank?
 	      
-	      uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
 	      puts('googleテスト2')
-          puts(JSON.parse(open(uri).read))
+	      
+	      #uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
+	      #uri = "https://www.googleapis.com/calendar/v3/calendars/watch
+	      
+          #puts(JSON.parse(open(uri).read))
   	      
 	      #ヘッダ取得
 	      #header1 = response.headers
