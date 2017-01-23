70eb5db4fe7cf65962817fd4dd2478622bed2399
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-23 17:22:23 +0900

    gg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,14 +15,20 @@ class WelcomeController < ApplicationController
 	  apikey = 'AIzaSyAC-jdWZDppBm8_cP1LRA79QeChfgPr9Qs'
 	  #calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
 	  calendarId = 'i8a77r26f9pu967g3pqpubv0ng@group.calendar.google.com'
-	  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
+	  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events.list?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
 	  
 	  #if !request.body.read.blank?
 	  if !open(uri).read.blank?
 	      
 	      puts('googleテスト2')
 	      #puts(open(uri).read)
-          puts(JSON.parse(open(uri).read))
+	      json_request = JSON.parse(open(uri).read)
+	      event_id = json_request["id"]
+	      
+          puts(json_request))
+  	      #puts(event_id)
+  	      
+  	      
   	      
 	      #ヘッダ取得
 	      #header1 = response.headers
