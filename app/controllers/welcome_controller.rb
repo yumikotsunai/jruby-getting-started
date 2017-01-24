c297b69da8bfc8ad150ecddc49afcd3288461692
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-24 09:32:26 +0900

    gg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,7 +15,8 @@ class WelcomeController < ApplicationController
 	  apikey = 'AIzaSyAC-jdWZDppBm8_cP1LRA79QeChfgPr9Qs'
 	  #calendarId = 'kke.co.jp_jh10o5p06igc6toeq8rerrbde8@group.calendar.google.com'
 	  calendarId = 'i8a77r26f9pu967g3pqpubv0ng@group.calendar.google.com'
-	  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events.list?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
+	  #uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
+	  uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/watch?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
 	  
 	  #if !request.body.read.blank?
 	  if !open(uri).read.blank?
