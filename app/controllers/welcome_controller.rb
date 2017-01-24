5accb7db7894bdfd154ab31bffd5f60c39b33c5c
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-24 17:13:38 +0900

    gg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'cgi'
 require 'json'
 require 'google/api_client'
+require 'http'
 
 class WelcomeController < ApplicationController
   
@@ -10,26 +11,44 @@ class WelcomeController < ApplicationController
   
   # GET /welcome
   def index
+  	  #必要なのがhttpsなのでSSLを有効にする。とりあえず証明書は無視。
+      ctx      = OpenSSL::SSL::SSLContext.new
+      ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      #postするデータを作成しておく
+      postbody = {
+	      "id": "abc",
+	      "type": "web_hook",
+	      "address": "https://whispering-harbor-83926.herokuapp.com/",
+	      }
+      }
+
+      #ヘッダーに認証用の情報をつけておく
+      #HTTP.post(URL)でURLにpostリクエストを送る（送ってresにレスポンスを取得。）
+      res = HTTP.headers("Content-Type" => "application/json",:Authorization => "Bearer 1/6qgSaD7fG01LXbnoptNfQCGi4XbfhQs1eScPhDnOEJg")
+      .post("https://www.googleapis.com/calendar/v3/calendars/my_calendar@gmail.com/events/watch", :ssl_context => ctx , :body => postbody.to_json)
+      
   	  
-  	  client = Google::APIClient.new
-	  client.authorization.client_id = '841258018012-jqn06q4ifmfvbj5ip42rvtemetcga7oj.apps.googleusercontent.com'
-	  client.authorization.client_secret = 'HuQ43i5_NiqOeOIZca4oJttJ'
-	  client.authorization.refresh_token = '1/6qgSaD7fG01LXbnoptNfQCGi4XbfhQs1eScPhDnOEJg'
-	  client.authorization.fetch_access_token!
-	  
-	  service = client.discovered_api('calendar', 'v3')
-	  
-	  client.execute!(
-	  	api_method: service.events.watch,
-	  	parameters: { calendarId: 'i8a77r26f9pu967g3pqpubv0ng@group.calendar.google.com' },
-	  	body_object: {
-	  		id: 'abc',
-	  	    type: 'web_hook',
-	  	    address: 'https://whispering-harbor-83926.herokuapp.com/'
-	  	}
-	  )
   	  
-  	  render
+  	  #client = Google::APIClient.new
+	  #client.authorization.client_id = '841258018012-jqn06q4ifmfvbj5ip42rvtemetcga7oj.apps.googleusercontent.com'
+	  #client.authorization.client_secret = 'HuQ43i5_NiqOeOIZca4oJttJ'
+	  #client.authorization.refresh_token = '1/6qgSaD7fG01LXbnoptNfQCGi4XbfhQs1eScPhDnOEJg'
+	  #client.authorization.fetch_access_token!
+	  
+	  #service = client.discovered_api('calendar', 'v3')
+	  
+	  #client.execute!(
+	  #	api_method: service.events.watch,
+	  #	parameters: { calendarId: 'i8a77r26f9pu967g3pqpubv0ng@group.calendar.google.com' },
+	  #	body_object: {
+	  #		id: 'abc',
+	  #	    type: 'web_hook',
+	  #	    address: 'https://whispering-harbor-83926.herokuapp.com/'
+	  #	}
+	  #)
+  	  
+  	  #render
   	  
 	  #uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events?orderBy=startTime&singleEvents=true&timeZone=Asia%2FTokyo&timeMin=#{CGI.escape(Time.now.iso8601)}&key=#{apikey}"
 	  #uri = "https://www.googleapis.com/calendar/v3/calendars/#{CGI.escape(calendarId)}/events/watch?key=#{apikey}"
