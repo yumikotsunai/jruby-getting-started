7dc8c79a6e1611abe501f843c7e0e6be20069f4e
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-25 10:42:00 +0900

    gg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,7 +27,7 @@ class WelcomeController < ApplicationController
       
       #ヘッダーに認証用の情報をつけておく
       #HTTP.post(URL)でURLにpostリクエストを送る（送ってresにレスポンスを取得。）
-      res = HTTP.headers("Content-Type" => "application/json",:Authorization => "Bearer 1/iN2JaZyApBk4VMGtp471fuWkFMrgl6TS8FBcWfU694I")
+      res = HTTP.headers("Content-Type" => "application/json",:Authorization => "Bearer ya29.GlveA0odIvUrHaAmHTstoQg4jxlEZwiblS6nP-4kauZw0z-DRnAjclRRKZNJjTJ0xs35ovEAbdnTytie2joXnwSCs2KNtTNeZGnhc-IfhWMeB2xGGK1cj7tQrRrH")
       .post("https://www.googleapis.com/calendar/v3/calendars/my_calendar@gmail.com/events/watch", :ssl_context => ctx , :body => postbody.to_json)
       
   	  puts(res)
