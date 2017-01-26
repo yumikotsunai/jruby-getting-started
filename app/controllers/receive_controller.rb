2e154a7f2083d10bce41a50113492a5f2f8a39b6
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-01-26 17:37:51 +0900

    gg

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -1,6 +1,9 @@
 
 class ReceiveController < ApplicationController
   
+  # この↓一文がないとCSRFチェックでこけるので、APIをやりとりしているControllerには必要
+  skip_before_filter :verify_authenticity_token
+  
   # GET/POST /webhook
   def webhook
   end
